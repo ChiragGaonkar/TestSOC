430d74b859b65c5954e43f82368a60d895718716
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-08 09:20:59 +0000

    Rename index for primary key constraint

--- SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA1.sql
+++ SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA1.sql
@@ -4,6 +4,6 @@ CREATE TABLE employees_schema1 (
     last_name VARCHAR2(50),
     CONSTRAINT pk_emp_schema1 PRIMARY KEY (emp_id)
     USING INDEX (
-        CREATE UNIQUE INDEX emp_pk_index ON employees_schema1(emp_id)
+        CREATE UNIQUE INDEX pk_emp_schema1 ON employees_schema1(emp_id)
     )
 );
