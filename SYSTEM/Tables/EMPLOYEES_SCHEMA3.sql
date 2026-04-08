ac64bec5331b87576acba18ef737bc8b6991576b
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-08 10:18:08 +0000

    Update EMPLOYEES_SCHEMA3.sql

--- SYSTEM/Tables/EMPLOYEES_SCHEMA3.sql
+++ SYSTEM/Tables/EMPLOYEES_SCHEMA3.sql
@@ -1,6 +1,7 @@
+-- Connect as second schema user
 CREATE TABLE "SYSTEM".employees_schema3 (
   emp_id NUMBER NOT NULL,
   first_name VARCHAR2(50 BYTE),
   last_name VARCHAR2(50 BYTE),
-  CONSTRAINT pk_emp_schema3 PRIMARY KEY (emp_id)
+  CONSTRAINT pk_emp_schema3 PRIMARY KEY (emp_id) USING INDEX "SYSTEM".emp_pk_index
 );
