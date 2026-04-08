2a7b5a45d03fcf0b53085d0a9d2a0f25271e2f38
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-08 09:26:08 +0000

    Update EMPLOYEES_SCHEMA2.sql

--- SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA2.sql
+++ SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA2.sql
@@ -2,5 +2,5 @@ CREATE TABLE softwarecompany.employees_schema2 (
   emp_id NUMBER NOT NULL,
   first_name VARCHAR2(50 BYTE),
   last_name VARCHAR2(50 BYTE),
-  CONSTRAINT pk_emp_schema2 PRIMARY KEY (emp_id) USING INDEX softwarecompany.emp_pk_index
+  CONSTRAINT pk_emp_schema2 PRIMARY KEY (emp_id)
 );
