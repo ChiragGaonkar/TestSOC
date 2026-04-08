1d049f89dccb0dff7518b143b80accd91b3ea867
Chirag Gaonkar <55709751+ChiragGaonkar@users.noreply.github.com>
2026-04-08 09:17:28 +0000

    Fix primary key constraint name in employees_schema1

--- SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA1.sql
+++ SOFTWARECOMPANY/Tables/EMPLOYEES_SCHEMA1.sql
@@ -2,7 +2,7 @@ CREATE TABLE employees_schema1 (
     emp_id NUMBER NOT NULL,
     first_name VARCHAR2(50),
     last_name VARCHAR2(50),
-    CONSTRAINT pk_emp_schema2 PRIMARY KEY (emp_id)
+    CONSTRAINT pk_emp_schema1 PRIMARY KEY (emp_id)
     USING INDEX (
         CREATE UNIQUE INDEX emp_pk_index ON employees_schema2(emp_id)
     )
